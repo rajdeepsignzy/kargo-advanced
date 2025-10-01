05b303ba7b6e320d1c79bc4ddb5803cf763aafe2
rajdeep.sharma <rajdeep.sharma@Rajdeep-MacBook-Pro-2.local>
2025-10-01 07:01:13 +0530

    add env var

---
+++ kind.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+set -x
+
+argo_cd_chart_version=8.5.8
+argo_rollouts_chart_version=2.40.4
+cert_manager_chart_version=1.18.2
+
+kind create cluster \
+  --wait 120s \
+  --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+name: kargo-quickstart
+nodes:
+- extraPortMappings:
+  - containerPort: 31443 # Argo CD dashboard
+    hostPort: 31443
+  - containerPort: 31444 # Kargo dashboard
+    hostPort: 31444
+  - containerPort: 31445 # External webhooks server
+    hostPort: 31445
+  - containerPort: 30081 # test application instance
+    hostPort: 30081
+  - containerPort: 30082 # UAT application instance
+    hostPort: 30082
+  - containerPort: 30083 # prod application instance
+    hostPort: 30083
+  
+EOF
+
+helm install cert-manager cert-manager \
+  --repo https://charts.jetstack.io \
+  --version $cert_manager_chart_version \
+  --namespace cert-manager \
+  --create-namespace \
+  --set crds.enabled=true \
+  --wait
+
+helm install argocd argo-cd \
+  --repo https://argoproj.github.io/argo-helm \
+  --version $argo_cd_chart_version \
+  --namespace argocd \
+  --create-namespace \
+  --set 'configs.secret.argocdServerAdminPassword=$2a$10$5vm8wXaSdbuff0m9l21JdevzXBzJFPCi8sy6OOnpZMAG.fOXL7jvO' \
+  --set dex.enabled=false \
+  --set notifications.enabled=false \
+  --set server.service.type=NodePort \
+  --set server.service.nodePortHttp=31443 \
+  --set server.extensions.enabled=true \
+  --set 'server.extensions.contents[0].name=argo-rollouts' \
+  --set 'server.extensions.contents[0].url=https://github.com/argoproj-labs/rollout-extension/releases/download/v0.3.3/extension.tar' \
+  --wait
+
+helm install argo-rollouts argo-rollouts \
+  --repo https://argoproj.github.io/argo-helm \
+  --version $argo_rollouts_chart_version \
+  --create-namespace \
+  --namespace argo-rollouts \
+  --wait
+
+# Password is 'admin'
+helm install kargo \
+  oci://ghcr.io/akuity/kargo-charts/kargo \
+  --namespace kargo \
+  --create-namespace \
+  --set api.service.type=NodePort \
+  --set api.service.nodePort=31444 \
+  --set api.adminAccount.passwordHash='$2a$10$Zrhhie4vLz5ygtVSaif6o.qN36jgs6vjtMBdM6yrU1FOeiAAMMxOm' \
+  --set api.adminAccount.tokenSigningKey=iwishtowashmyirishwristwatch \
+  --set externalWebhooksServer.service.type=NodePort \
+  --set externalWebhooksServer.service.nodePort=31445 \
+  --wait
+
+
+
+# kargo login https://localhost:31444 --admin --insecure-skip-tls-verify
+# argocd login localhost:31443 --username admin --password admin  --insecure
+
+kargo create credentials github-creds \
+  --project kargo-advanced \
+  --git \
+  --username rajdeepsignzy \
+  --repo-url https://github.com/rajdeepsignzy/kargo-advanced.git
+
+docker buildx imagetools create \
+  ghcr.io/akuity/guestbook:latest \
+  -t ghcr.io/rajdeepsignzy/guestbook:v0.0.2
